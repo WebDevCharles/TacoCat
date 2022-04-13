3894c9f9283d40fc0bf401f16e8e4faa8618f308
Charles Hall <webdevcharles@gmail.com>
2022-04-13 11:22:10 -0400

    JS updates

--- js/site.js
+++ js/site.js
@@ -9,8 +9,10 @@ function getMessage() {
     } else {
         // Changing output to "lowercase" regardless of input.
         msg = msg.toLowerCase();
-        // Remove all "special" characters.
-        msg = msg.replace(/[^a-zA-Z0-9]/g, '');
+        // Declaring regex as a constant to prevent modifcation.
+        const regex = /[^a-zA-Z0-9]/gi;
+        msg = msg.replace(regex, '');
+
         let palinObj = reverseMessage(msg);
         displayMessage(palinObj);
     }
@@ -24,26 +26,26 @@ function reverseMessage(originalWord) {
 
     revWord = originalWord.split("").reverse().join("");
 
-}
 
-if (originalWord == revWord) {
-    returnObj.message = `The word/ phrase is a palindrome.`
-    returnObj.IsPalindrome = true;
-} else {
-    returnObj.message = `The word/ phrase is not a palindrome.`
-    returnObj.IsPalindrome = false;
-}
+    if (originalWord == revWord) {
+        returnObj.message = `The word/phrase is a palindrome!`
+        returnObj.IsPalindrome = true;
+    } else {
+        returnObj.message = `The word/phrase is not a palindrome!`
+        returnObj.IsPalindrome = false;
+    }
 
-returnObj.originalWord = originalWord;
-returnObj.reversedWord = revWord;
+    returnObj.originalWord = originalWord;
+    returnObj.reversedWord = revWord;
 
-return returnObj;
+    return returnObj;
+
 }
 
 // Final Step - View
 function displayMessage(palinObj) {
 
     element = document.getElementById("results");
-    element.innerHTML = palinObj.message;
+    element.innerHTML = `${palinObj.message} - ${palinObj.originalWord}`;
 
 }
